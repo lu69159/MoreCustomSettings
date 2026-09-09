20ee4d070b2b311c097f915de033aba21b67a6ac
lu69159 <2909165527@qq.com>
2026-09-09 21:37:24 +0800

    移动端导入菜单音乐BUG

--- src/MCS/game/CustomMusicLoader.java
+++ src/MCS/game/CustomMusicLoader.java
@@ -221,7 +221,7 @@ public class CustomMusicLoader{
                     }
 
                     fi.copyTo(folder);
-                    Path source = Paths.get(fi.path());
+                    Path source = Paths.get(folder.path() + "/" + fi.name());
                     Path to = Paths.get(folder.path() + "/" + realString(inputName) + "__" + fi.length() + "." + fi.extension());
                     Files.move(source, to, StandardCopyOption.REPLACE_EXISTING);
                     successImported = true;
